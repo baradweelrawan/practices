b12036b4121547056b228b73e71beb15eda4a817
baradweelrawan <bardaweel_rawan@yahoo.com>
2021-04-30 16:36:30 +0300

    add star

---
+++ js/parc.js
@@ -0,0 +1,21 @@
+'user strict'
+let level =prompt('please enter the level of row you want');
+//let j = promot('please enter *');
+//function start()
+//{
+    for (let i=1; i <=level ; i++ )  
+    {
+     //   if (i ===level)
+      //  {
+            for(let j=1 ; j <= i ; j++)
+            {
+               //let j = promot('please enter *')
+              document.write('*')
+              
+              // document.write('<br />')
+            }
+      //  }
+      //document.write('*')
+      document.write('<br />')
+    }
+//}
